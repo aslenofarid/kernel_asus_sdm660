545c2b0b04e6a5514be7f55da3e6dde3c5dc168f
aslenofarid <yoniasleno.farid14@gmail.com>
2023-08-03 14:30:55 +0000

    fs: Patch KernelSU
    Signed-off-by: aslenofarid <yoniasleno.farid14@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1536,6 +1536,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1546,6 +1549,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
